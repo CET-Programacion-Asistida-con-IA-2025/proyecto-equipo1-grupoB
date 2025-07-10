2b106106744d53c06c75f98f846f53a7c0c5f7d8
Melinavaccaro <melinavaccaro17@gmail.com>
2025-07-10 13:42:19 -0300

    cambios en el css

---
+++ Práctica.js
@@ -0,0 +1,8 @@
+let nombre="Melina"
+let nombreusuario= prompt("Ingrese su nombre de usuario:");
+while (nombreusuario== nombre){
+    alert("el nombre de usuario puede pasar");
+Else;
+    alert("el nombre de usuaruio no puede pasar, intente de nuevo");
+}
+ 
